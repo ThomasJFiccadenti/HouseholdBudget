2a0cec006b78ef3f5ef9188f41b35112f62a19c5
Thomas Ficcadenti <thomasJficcadenti@gmail.com>
2026-06-24 22:02:44 -0700

    Add read input config csv

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-print("hello world")
+import csv
+
+config = "./input/config.csv"
+
+with open(config) as config_file:
+    config_reader = csv.reader(config_file)
+    next(config_reader)  # skip header
+    for row in config_reader:
+        print(row)
