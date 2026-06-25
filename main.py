9c80155148e58cb927b810ebf2118a6bd03a686b
Thomas Ficcadenti <thomasJficcadenti@gmail.com>
2026-06-24 22:11:45 -0700

    Create function that reads cards file and outputs list of cards

--- main.py
+++ main.py
@@ -1,9 +1,20 @@
 import csv
 
-config = "./input/config.csv"
+CARDS_CSV = "./input/cards.csv"
+STATEMENT_DIR = "./input/"
 
-with open(config) as config_file:
-    config_reader = csv.reader(config_file)
-    next(config_reader)  # skip header
-    for row in config_reader:
-        print(row)
+
+def get_cards(cards_csv_path: str):
+    out = []
+    with open(cards_csv_path) as config_file:
+        config_reader = csv.reader(config_file)
+        next(config_reader)  # skip header
+        for row in config_reader:
+            out.append({"card_no": row[0], "card_name": row[1]})
+
+    return out
+
+
+cards = get_cards(CARDS_CSV)
+print(cards[0]['card_no'])
+#print(cards[0].card_no, cards[0].card_name)
