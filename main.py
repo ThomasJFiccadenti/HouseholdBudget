84eb1568c85c2278c72af03f25274b13dc3b2672
Thomas Ficcadenti <thomasJficcadenti@gmail.com>
2026-06-26 10:07:50 -0700

    Experimenting with Tkinter

--- main.py
+++ main.py
@@ -1,20 +1,26 @@
-import csv
+import tkinter as tk
 
-CARDS_CSV = "./input/cards.csv"
-STATEMENT_DIR = "./input/"
+root = tk.Tk()
+
+root.title("Budget App")
+root.minsize(200, 200)
+root.maxsize(500, 500)
+root.geometry("300x300+50+50")
+
+tk.Label(root, text="Nothing will work unless you do.").pack()
+tk.Label(root, text="- Maya Angelou").pack()
+
+root.mainloop()
+# CARDS_CSV = "./input/cards.csv"
+# STATEMENT_DIR = "./input/"
 
 
-def get_cards(cards_csv_path: str):
-    out = []
-    with open(cards_csv_path) as config_file:
-        config_reader = csv.reader(config_file)
-        next(config_reader)  # skip header
-        for row in config_reader:
-            out.append({"card_no": row[0], "card_name": row[1]})
+# def get_cards(cards_csv_path: str):
+#     out = []
+#     with open(cards_csv_path) as config_file:
+#         config_reader = csv.reader(config_file)
+#         next(config_reader)  # skip header
+#         for row in config_reader:
+#             out.append({"card_no": row[0], "card_name": row[1]})
 
-    return out
-
-
-cards = get_cards(CARDS_CSV)
-print(cards[0]['card_no'])
-#print(cards[0].card_no, cards[0].card_name)
+#     return out
